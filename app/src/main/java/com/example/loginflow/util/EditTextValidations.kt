6ddae1efdb34b71c57ddd2623b57c25298b176dc
Adarsh Latiyan <adarshlatiyan@gmail.com>
2020-11-26 18:21:14 +0530

    Adds recharge screen layout.

--- app/src/main/java/com/example/loginflow/util/EditTextValidations.kt
+++ app/src/main/java/com/example/loginflow/util/EditTextValidations.kt
@@ -1,7 +1,6 @@
 package com.example.loginflow.util
 
 import android.text.InputType
-import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.textfield.TextInputEditText
 
 fun TextInputEditText.validate(): Boolean {
@@ -9,7 +8,6 @@ fun TextInputEditText.validate(): Boolean {
     when (inputType) {
         InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS -> {
             if (input.isEmpty()) {
-//                setError("Email can not be empty", R.drawable.ic_warning)
                 error = "Email can not be empty"
                 return false
             }
@@ -17,18 +15,10 @@ fun TextInputEditText.validate(): Boolean {
 
         InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD -> {
             if (input.isEmpty()) {
-//                setError("Password can not be empty", R.drawable.ic_warning)
                 error = "Password can not be empty"
                 return false
             }
         }
     }
     return true
-}
-
-private fun TextInputEditText.setError(error: String, id: Int) {
-    setError(
-        error,
-        ResourcesCompat.getDrawable(resources, id, null)
-    )
 }
